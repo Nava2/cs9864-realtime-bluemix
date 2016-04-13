eb5a8fd72695a970ed1a4bbe03bab94fa67b7a04
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-04-13 17:24:17 -0400

    Implement Stock Streaming Service
    Implements a service that will stream stocks to other services. The
    service will live at http://stock-data-handler-cs9864-2016.mybluemix.net.
    
    Closes #26

---
+++ stock-client/routes/register.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const url = require('url');
+
+const _ = require('lodash');
+const express = require('express');
+
+module.exports = (winston) => {
+  const w = (!!winston ? winston : require('winston'));
+  if (!!process.env['LOG_LEVEL']) {
+    w.level = process.env['LOG_LEVEL'];
+  }
+
+  const router = express.Router();
+  const EndPoint = require('../lib/endpoint')(w);
+
+  router.put('/register', function(req, res) {
+    const ep = new EndPoint({
+      href: (!_.isString(req.body.href) ? url.format(_.extend(req.body.href, { hostname: req.ip })) : req.body.href),
+      verb: req.body.verb
+    });
+
+    this.locals.mgr.addEndPoint({
+      tickers: req.body.tickers,
+      endpoint: ep
+    }, err => {
+      if (!!err) {
+        res.status(403).json({
+          success: false,
+          error: err.error
+        });
+      } else {
+        w.debug(`app.js: Registered ${ep.toString()}`);
+
+        res.json({
+          success: true
+        });
+      }
+    });
+  });
+
+  router.delete('/register', function(req, res) {
+    const ep = new EndPoint({
+      href: (!_.isString(req.body.href) ? url.format(_.extend(req.body.href, { hostname: req.ip })) : req.body.href),
+      verb: req.body.verb
+    });
+
+    this.locals.mgr.removeEndpoint(ep, err => {
+      if (!!err) {
+        res.status(403).json({
+          success: false,
+          error: err.error
+        });
+      } else {
+        w.debug(`app.js: Unregistered ${ep.toString()}`);
+        res.json({
+          success: true
+        });
+      }
+    });
+  });
+
+  return router;
+};
+
