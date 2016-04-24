1e48f9243e234a68166db988c7e7b20bb8609f42
Alex <alheure2@gmail.com>
2016-04-24 11:37:37 -0400

    Changed the format of the response to include news link

--- data-yahoo-rss/app.js
+++ data-yahoo-rss/app.js
@@ -9,7 +9,7 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-
+//Setup RSS Feed Parser
 
 parser = require('parse-rss');
 
@@ -25,55 +25,63 @@ var appEnv = cfenv.getAppEnv();
 //get the Data from the feed and through the rss to JSON translator.
 // Pass in stock and callback function
 function getData (str,callback) {
-  url='http://finance.yahoo.com/rss/headline?s='+str;
- parser(url,function (err, rss) {
-     if (rss==null) {
-      console.log("An error has occured. Abort everything!");
-         console.log(err);
-      callback(null);
-    }
-     else {
-        var keys = Object.keys(rss);
-        var result = [];
-        for (var i = 0, length = keys.length; i < length; i++) {
-
-            dat = new Date(Date.parse(rss[i].date));
-
-            result.push({title: rss[i].title, date: dat});
-
+    url='http://finance.yahoo.com/rss/headline?s='+str;
+    parser(url,function (err, rss) {
+        if (rss==null) {
+            console.log("An error has occured. Abort everything!");
+            console.log(err);
+            callback(null);
         }
-        callback(result);
-     }
-  });
+        else {
+            var keys = Object.keys(rss);
+            var result = [];
+            for (var i = 0, length = keys.length; i < length; i++) {
+
+                // Get the link from the substring
+                a=rss[i].link.indexOf('*');
+                b=rss[i].link.length;
+                var str=rss[i].link;
+                var res=str.substring(a+1,b);
+
+                // Make the date field a Date object
+                dat = new Date(Date.parse(rss[i].date));
+
+                //Add post to the array
+                result.push({guid: rss[i].guid,title: rss[i].title, date: dat,link:res});
+
+            }
+            callback(result);
+        }
+    });
 }
 
 // //Example : http://localhost:6003/getData?stockname=AAPL
 app.get('/getData', function (req, res) {
-  stock = req.query.stockname;
-  console.log(stock);
-  getData(stock,function(data) {
-      if(data!==null) {
-          var int =data.filter(function(n){
-              var b = new Date();
-              if(n.date.getDate()==b.getDate() && n.date.getYear()==b.getYear()){
-                  return n;
-              }
-          });
-          if(int.length==0){
-              res.json("no data");
-          }
-          else {
-              res.json(int);
-          }
+    stock = req.query.stockname;
 
-      }
-      else{res.status(400).send({ error: 'Invalid Stock' });}
-  });
+    getData(stock,function(data) {
+        if(data!==null) {
+            var int =data.filter(function(n){
+                var b = new Date();
+                if(n.date.getDate()==b.getDate() && n.date.getYear()==b.getYear()){
+                    return n;
+                }
+            });
+            if(int.length==0){
+                res.json("no data");
+            }
+            else {
+                res.json(int);
+            }
+
+        }
+        else{res.status(400).send({ error: 'Invalid Stock' });}
+    });
 });
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, function () {
 
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
