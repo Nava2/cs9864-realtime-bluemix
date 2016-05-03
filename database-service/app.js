074d61f191f92b88981010c9650d224fe29dd182
Alex <alheure2@gmail.com>
2016-05-02 20:10:20 -0400

    Addressed the comments for the database service

--- database-service/app.js
+++ database-service/app.js
@@ -30,12 +30,6 @@ var api = require('./routes/api')(w);
 var app = express();
 
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('combined', {
   skip: function (req, res) { return res.statusCode < 400 }
 }));
@@ -49,35 +43,35 @@ app.use(compression());
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(config.port, () => {
   w.info("express started!");
@@ -103,4 +97,4 @@ app.listen(config.port, () => {
   });
 
 });
-module.exports = app;
+
